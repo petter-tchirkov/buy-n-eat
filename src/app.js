a607fc5350e4c90ef7badbf7869d152c7d26f4a7
pete <tchirkov.petter@gmail.com>
2021-03-20 20:51:12 +0200

    adaptive

--- src/app.js
+++ src/app.js
@@ -15,9 +15,22 @@ hello();
 
 document.addEventListener("DOMContentLoaded", function changeSrc() {
   let cardPhoto = document.querySelector(".bonus--img img");
+  let clubCard = document.querySelector(".club--card img");
   console.log(cardPhoto);
   console.log(window.innerWidth);
-  if (window.innerWidth <= 1280) {
+  if (window.innerWidth <= 1200 && window.innerWidth >= 992) {
+    clubCard.src = "/assets/images/card-vertical.png";
+  }
+  if (window.innerWidth <= 1200) {
     cardPhoto.src = "/assets/images/card-vertical.png";
   }
+
+  let feature = document.querySelector(".features");
+
+  document.querySelector(".main--btn").addEventListener("click", () => {
+    feature.scrollIntoView({
+      block: "start",
+      behavior: "smooth",
+    });
+  });
 });
